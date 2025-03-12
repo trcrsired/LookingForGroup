d0c42968669504a2a215253ba71da63f530deb05
trcrsired <uwgghhbcad@gmail.com>
2025-03-12 10:03:16 -0400

    fix up

--- Options/Core.lua
+++ Options/Core.lua
@@ -1,6 +1,11 @@
 local AceAddon = LibStub("AceAddon-3.0")
 local LookingForGroup = AceAddon:GetAddon("LookingForGroup")
 local LookingForGroup_Options = AceAddon:GetAddon("LookingForGroup_Options")
+
+local C_AddOns = C_AddOns
+if C_AddOns == nil then
+	C_AddOns = _G
+end
 local LoadAddOn = C_AddOns.LoadAddOn
 local GetNumAddOns = C_AddOns.GetNumAddOns
 local GetAddOnMetadata = C_AddOns.GetAddOnMetadata
